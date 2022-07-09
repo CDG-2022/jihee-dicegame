0b98b727e12eca52860f581e2aba6eaa49187f9e
GHeeJeon <jhjeon_99@naver.com>
2022-07-10 00:49:59 +0900

    update Dice, FraudDice

--- DiceGame/src/com/example/dicegame/Dice.java
+++ DiceGame/src/com/example/dicegame/Dice.java
@@ -1,4 +1,14 @@
 package com.example.dicegame;
 
+import java.util.Random;
+
 public class Dice {
+    Random random = new Random();
+
+    int roll(){
+        int diceNumber;
+
+        diceNumber = random.nextInt(6) + 1; // 0 ~ 5 에 1 을 더해 1 ~ 6 을 만든다.
+        return diceNumber;
+    }
 }
--- DiceGame/src/com/example/dicegame/FraudDice.java
+++ DiceGame/src/com/example/dicegame/FraudDice.java
@@ -1,4 +1,32 @@
 package com.example.dicegame;
 
+import java.util.Random;
+
 public class FraudDice {
+    Random random = new Random();
+
+    int roll(){
+        int diceNumber;
+
+        diceNumber = random.nextInt(6) + 1; // 0 ~ 5 에 1 을 더해 1 ~ 6 을 만든다.
+        return diceNumber;
+    }
+
+    int rollEasy(){
+        int diceNumber;
+
+        diceNumber = random.nextInt(6) + 1;
+        while(diceNumber >= 5) // 5 이상의 눈금이 나오면 다시 던집니다.
+            diceNumber = random.nextInt(6) + 1;
+        return diceNumber;
+    }
+
+    int rollHard(){
+        int diceNumber;
+
+        diceNumber = random.nextInt(6) + 1;
+        while(diceNumber <= 2) // 2 이하의 눈금이 나오면 다시 던집니다.
+            diceNumber = random.nextInt(6) + 1;
+        return diceNumber;
+    }
 }
