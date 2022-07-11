cb9d35a194d3a617e4dd76d8469520bae14e3c4c
GHeeJeon <jhjeon_99@naver.com>
2022-07-12 02:09:55 +0900

    Update FraudPlayer.java
    주석 키워드 변경

--- DiceGame/src/player/FraudPlayer.java
+++ DiceGame/src/player/FraudPlayer.java
@@ -15,8 +15,8 @@ public class FraudPlayer {
     }
 
     int levelOfDice() {
-        // (플레이어의 total > 사기 플레이어의 total) then 사기 주사위 하드모드
-        // (사기 플레이어의 total - 플레이어의 total > 6) then 사기 주사위 이지모드
+        // (플레이어의 total > 사기 플레이어의 total) then 사기 주사위 rollHard()
+        // (사기 플레이어의 total - 플레이어의 total > 6) then 사기 주사위 rollEasy()
         // else 사기 주사위 노말모드
         return 0;
     }
