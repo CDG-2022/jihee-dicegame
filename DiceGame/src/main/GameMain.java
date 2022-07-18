bd2aa3315885468ae7db95d298f8df839cf137ba
GHeeJeon <jhjeon_99@naver.com>
2022-07-19 03:11:45 +0900

    main-apply lombok, change log position

--- DiceGame/src/main/GameMain.java
+++ DiceGame/src/main/GameMain.java
@@ -1,6 +1,9 @@
 package main;
 
+import dice.Dice;
+import dice.FraudDice;
 import judge.Judge;
+import lombok.*;
 import player.FraudPlayer;
 import player.Player;
 import recorder.Recorder;
@@ -8,18 +11,31 @@ import recorder.Recorder;
 import java.util.Scanner;
 
 public class GameMain {
+    @Getter
+    @Setter
+
     public static final int five = 5;
-    public static void main(){
+    public static void main(String[] args){
         String playerName1, playerName2;
 
         Scanner scanner = new Scanner(System.in);
+
+        Dice dice = new Dice();
+
         System.out.print("첫 번째 플레이어의 이름은 : ");
         playerName1 = scanner.nextLine();
+        Player player = new Player(playerName1);
+
+        //player.setDice(dice1);
+        //System.out.println(player.getDice());
+        
+        FraudDice fraudDice = new FraudDice();
+
         System.out.print("두 번째 플레이어의 이름은 : ");
         playerName2 = scanner.nextLine();
 
-        Player player = new Player(playerName1);
         FraudPlayer fraudPlayer = new FraudPlayer(playerName2);
+
         Judge judge = new Judge();
         Recorder recorder = new Recorder();
     }
--- DiceGame/src/player/Player.java
+++ DiceGame/src/player/Player.java
@@ -1,16 +1,20 @@
 package player;
 
 import dice.Dice;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Player {
+
     public String name;
     int total;
+    Dice dice;
 
     public Player(String name){
         this.name = name;
         System.out.println("플레이어 등장! 내 이름은 " + name);
         System.out.println("나는 일반 주사위를 가지고 있지!");
-        Dice dice = new Dice();
-        this.total += dice.roll();
+        this.dice = null;
+        //this.total += dice.roll();
     }
 }
